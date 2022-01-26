534be8a031e6c700b5a752cf9d6a4b6ed36e8f92
Rogers <Owen.Rogers@RJUHSD.INT>
2022-01-26 11:49:08 -0800

    User can make their birthday

--- birthday.py
+++ birthday.py
@@ -1,4 +1,11 @@
 print("Welcome.")
 
 for i in range(500):
-    print("james is a loser")
+    print("khang is cool")
+
+
+m = input("Birthday month: ")
+d = input("Birthday year: ")
+y = input("Birthday day: ")
+
+print("You were born on", m, d, y)
